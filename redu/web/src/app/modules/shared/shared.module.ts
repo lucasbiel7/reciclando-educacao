af977fbf13bce43bc5fbfc209befec736bc5088f
lucasbiel7 <lucasbiel7@icloud.com>
2018-10-01 00:08:30 -0300

    Iniciando página web, tentando ajusta a página com o layout proposto pelo Vinicius

--- redu/web/src/app/modules/shared/shared.module.ts
+++ redu/web/src/app/modules/shared/shared.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
 @NgModule({
-  exports: [
-    CommonModule,
-    RouterModule,
-    MenuComponent,
-    RodapeComponent,
-  ],
-  declarations: [MenuComponent, RodapeComponent]
+    exports: [
+        CommonModule,
+        RouterModule,
+        MenuComponent,
+        RodapeComponent,
+        AngularFontAwesomeModule,
+    ],
+    imports: [
+        AngularFontAwesomeModule
+    ],
+    declarations: [
+        MenuComponent,
+        RodapeComponent
+    ]
 })
 export class SharedModule { }
---
+++ redu/web/src/environments/environment.dese.ts
@@ -0,0 +1,9 @@
+/**
+ * Enviroment criado para ambiente de desenvolvimento
+ *
+ */
+export const environment = {
+  production: false,
+  version: '0.0.1',
+  backend: 'localhost:8180/redu/rest'
+};
