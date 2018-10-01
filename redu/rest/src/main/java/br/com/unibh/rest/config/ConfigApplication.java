3ea8d19dac7bad73e58b4c9e91e090faa2b31883
lucasbiel7 <lucasbiel7@icloud.com>
2018-09-30 21:12:22 -0300

    Finalizando configuração do REST

---
+++ redu/rest/src/main/java/br/com/unibh/rest/config/ConfigApplication.java
@@ -0,0 +1,15 @@
+package br.com.unibh.rest.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configurando a parte de REST da aplicação
+ * 
+ * @author Lucas Gabriel
+ *
+ */
+@ApplicationPath("")
+public class ConfigApplication extends Application {
+
+}
