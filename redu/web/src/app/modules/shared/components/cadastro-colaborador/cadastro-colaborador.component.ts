91552dae29d769d8c3903747f0ce5071be399951
lucasbiel7 <lucasbiel7@icloud.com>
2018-10-09 22:53:02 -0300

    Ajuste do componente de stepper

--- redu/web/src/app/modules/shared/components/cadastro-colaborador/cadastro-colaborador.component.ts
+++ redu/web/src/app/modules/shared/components/cadastro-colaborador/cadastro-colaborador.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatStepper } from '@angular/material';
 
 @Component({
-  selector: 'redu-cadastro-colaborador',
-  templateUrl: './cadastro-colaborador.component.html',
-  styleUrls: ['./cadastro-colaborador.component.scss']
+    selector: 'redu-cadastro-colaborador',
+    templateUrl: './cadastro-colaborador.component.html',
+    styleUrls: ['./cadastro-colaborador.component.scss']
 })
 export class CadastroColaboradorComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('stepper')
+    public stepper: MatStepper;
+    currentStep: any;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.currentStep = 0;
+    }
 
 }
