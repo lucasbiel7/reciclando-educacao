113be2328e99c337862d85e12f2020b6958317b9
lucasbiel7 <lucasbiel7@icloud.com>
2018-10-09 23:25:42 -0300

    Adicionando getters and setters na DTO de usuário

--- redu/service/negocio/src/main/java/br/com/unibh/negocio/dto/UsuarioResource.java
+++ redu/service/negocio/src/main/java/br/com/unibh/negocio/dto/UsuarioResource.java
@@ -17,4 +17,36 @@ public class UsuarioResource extends BaseResponse {
 
 	private String senha;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 }
