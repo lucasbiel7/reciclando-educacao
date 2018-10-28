bba7ac933837a63132e6c1e70d4cf4874108d0bc
ViniciusMenezesLopes <Vmenezes59@gmail.com>
2018-10-28 20:13:06 -0300

    Criei a tela de Colaborador.

--- redu/web/src/app/modules/inicio/components/formulario-colaborador/formulario-colaborador.component.ts
+++ redu/web/src/app/modules/inicio/components/formulario-colaborador/formulario-colaborador.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Validator } from '../../../shared/resource/class/validator.class';
 
 @Component({
-  selector: 'redu-formulario-colaborador',
-  templateUrl: './formulario-colaborador.component.html',
-  styleUrls: ['./formulario-colaborador.component.scss']
+    selector: 'redu-formulario-colaborador',
+    templateUrl: './formulario-colaborador.component.html',
+    styleUrls: ['./formulario-colaborador.component.scss']
 })
 export class FormularioColaboradorComponent implements OnInit {
+    public formulario: FormGroup;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+
+
+
+    constructor() {
+        this.formulario = new FormGroup({
+            nomeColaborador: new FormControl('', Validators.required),
+            tipoColaborador: new FormControl('', Validators.required),
+            tipoFormacao: new FormControl('', Validators.required),
+            tipoExperiencia: new FormControl('', Validators.required)
+        });
+    }
+
+    ngOnInit() {
+        this.formulario.get('nomeColaborador').hasError('required');
+    }
+
+    public cadastroColaborador() {
+        Validator.validarCampos(this.formulario);
+        if (this.formulario.valid) {
+        }
+
+    }
+
 
 }
+
+
+
