1aa1eadd44edbc19ff39d1c27d3363e79fbc7188
ViniciusMenezesLopes <Vmenezes59@gmail.com>
2018-11-10 19:39:36 -0200

    Fiz mais validações e o campo tipo colaborador puxando do banco

--- redu/web/src/app/modules/inicio/components/formulario-colaborador/formulario-colaborador.component.ts
+++ redu/web/src/app/modules/inicio/components/formulario-colaborador/formulario-colaborador.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Validator } from '../../../shared/resource/class/validator.class';
+import { TipoColaborador } from '../../../shared/resource/class/tipo-colaborador.class';
+import { ColaboradorService } from '../../../shared/services/colaborador.service';
+import { Colaborador } from '../../../shared/resource/class/colaborador.class';
 
 @Component({
     selector: 'redu-formulario-colaborador',
@@ -9,22 +12,26 @@ import { Validator } from '../../../shared/resource/class/validator.class';
 })
 export class FormularioColaboradorComponent implements OnInit {
     public formulario: FormGroup;
+    public tiposDeColaborador: TipoColaborador[];
+    public colaborador: Colaborador;
 
 
 
-
-
-    constructor() {
+    constructor(private colaboradorService: ColaboradorService) {
         this.formulario = new FormGroup({
             nomeColaborador: new FormControl('', Validators.required),
             tipoColaborador: new FormControl('', Validators.required),
             tipoFormacao: new FormControl('', Validators.required),
             tipoExperiencia: new FormControl('', Validators.required)
         });
+        this.colaborador = new Colaborador();
     }
 
     ngOnInit() {
         this.formulario.get('nomeColaborador').hasError('required');
+        this.colaboradorService.pegarTiposColaboradores().subscribe(tiposColaborador => {
+            this.tiposDeColaborador = tiposColaborador;
+        });
     }
 
     public cadastroColaborador() {
