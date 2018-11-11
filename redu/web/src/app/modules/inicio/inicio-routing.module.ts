234d5a74ff2e05cea328f07a83981e02a35cb5b2
lucasbiel7 <lucasbiel7@icloud.com>
2018-11-11 21:14:05 -0200

    Ajuste para quando redirecionar para inicio apenas quando não estiver logado

--- redu/web/src/app/modules/inicio/inicio-routing.module.ts
+++ redu/web/src/app/modules/inicio/inicio-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial.component';
 import { SobreComponent } from './components/sobre/sobre.component';
+import { UsuarioNaoAutenticadoGuard } from '../shared/services/usuario-nao-autenticado.guard';
 
 const routes: Routes = [
 
     {
         path: 'inicio',
         component: PaginaInicialComponent,
+        canActivate: [UsuarioNaoAutenticadoGuard]
     },
     {
         path: 'sobre',
