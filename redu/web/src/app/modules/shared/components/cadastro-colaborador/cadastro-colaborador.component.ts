9c68570c98f3184fdc70694b9d74cc47ed8109a3
lucasbiel7 <lucasbiel7@icloud.com>
2018-11-11 20:42:28 -0200

    Ajuste

--- redu/web/src/app/modules/shared/components/cadastro-colaborador/cadastro-colaborador.component.ts
+++ redu/web/src/app/modules/shared/components/cadastro-colaborador/cadastro-colaborador.component.ts
@@ -57,7 +57,7 @@ export class CadastroColaboradorComponent implements OnInit, AfterViewInit {
             senha: new FormControl('', [Validators.required]),
             confirmarSenha: new FormControl('', [Validators.required])
         });
-        this.colaborador = new PessoaJuridica();
+        this.colaborador = new Colaborador();
     }
 
 
