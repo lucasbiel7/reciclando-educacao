002bb8e86a6a916cf83af294f37faeb89fb1af9d
protoman555 <diego.soares1992@hotmail.com>
2018-11-17 17:38:02 -0200

    Alterações da áre do usuario e perguntas frequentes.

--- redu/web/src/app/modules/principal/components/area-do-usuario/area-do-usuario.component.ts
+++ redu/web/src/app/modules/principal/components/area-do-usuario/area-do-usuario.component.ts
@@ -15,3 +15,11 @@ export class AreaDoUsuarioComponent implements OnInit {
     }
 
 }
+export class AreaDoUsuario {
+    tiles = [
+        { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+        { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+        { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+        { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+    ];
+}
